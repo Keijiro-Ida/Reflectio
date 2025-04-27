e4612631fce8aded90a4cef3f5a7ecabfd70b264
Keijiro_Ida <idatt1122@gmail.com>
2025-04-27 10:22:00 +0900

    cors変更

--- src/config/cors.php
+++ src/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
